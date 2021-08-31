a0bf71770ebe76b0e36053f49e0bc7085dd62e55
Aarushi853 <89862967+Aarushi853@users.noreply.github.com>
2021-08-31 23:08:44 +0530

    Created positivenumbers.py

---
+++ positivenumbers.py
@@ -0,0 +1,17 @@
+mylist = [12,25,-45,55,-12,-41]
+
+for i in mylist: 
+    if i>0: 
+        print(i,end=", ")
+        
+        
+        
+    mylist = [12,-45,55,-41]
+anslist = []
+
+for i in mylist: 
+    if i>0:
+        anslist.append(i)
+       # print(i,end=", ") 
+
+print (anslist)    
